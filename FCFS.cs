ef8082d373c2735cbb9d8761a58b303f548c915d
Dan-o <dferguson992@gmail.com>
2013-11-04 18:39:45 -0500

    Update FCFS.cs

--- FCFS.cs
+++ FCFS.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 /// AUTHOR: COLEEN FITZSIMONS
 
@@ -16,13 +17,14 @@ namespace COM310JobScheduler
         /// </summary>
         /// <param name="activeProcesses">LIST OF ALL PROCESSES THAT ARE ACTIVE</param>
         /// <returns>RETURNS THE STRING REPRESENTATION OF A GANTT CHART BUILT BY A STRINGBUILDER</returns>
-        
+        public override string calculateGantt(List<Process> activeProcesses)
+        {
             StringBuilder gantt = new StringBuilder();
             int totalWaitTime = 0;
             sortArrival(ref activeProcesses);
             bool complete = false;
             int i = 0;
-            while(!complete)
+            while (!complete)
             {
                 if (activeProcesses[i++].BurstTime == 999)
                 {
@@ -43,11 +45,11 @@ namespace COM310JobScheduler
                             ///of this prgram, we are having all of the processes arrive at the same time.
                             ///The 'first come first serve' order is being determined by the user's input,
                             ///not the actual arrival of the processes.
-                            ///This allows us to use the same algorithm to determine the total wait time and 
-                            ///turn around times of each process without having to alter the code.  
+                            ///This allows us to use the same algorithm to determine the total wait time and
+                            ///turn around times of each process without having to alter the code.
                             ///If we were to ask for the arrival times, we would first sort the arrival
-                            ///then we would find the difference between the arrival and the wait time to 
-                            ///determine the actual wait time for each process.  We would then use the actual 
+                            ///then we would find the difference between the arrival and the wait time to
+                            ///determine the actual wait time for each process. We would then use the actual
                             ///wait time to help us determine the turn around time for the process as well.
                             p.WaitTime = totalWaitTime;
                             totalWaitTime += p.BurstTime;
@@ -62,9 +64,15 @@ namespace COM310JobScheduler
             gantt.Append("--------------------\n\n");
             return gantt.ToString();
         }
-        public override string calculateGantt(List<Process> activeProcesses)
+
+        /// <summary>
+        /// CALLS BASE METHOD TO ADD GANTT CHART INFO TO A STRINGBUILDER
+        /// </summary>
+        /// <param name="p">THE REFERENCED PROCESS</param>
+        /// <param name="g">THE STRING BUILDER OBJECT BEING ADDED TO</param>
+        private new void buildGantt(Process p, ref StringBuilder g)
         {
-            return base.buildGantt(p, ref g);
+            base.buildGantt(p, ref g);
         }
     }
 }
